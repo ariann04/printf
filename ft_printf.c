865632ef8aabe55780b433556d07a18e651d7352
Tsvetelina Blagoeva Mihaylova <tblagoev@c3r8s4.42madrid.com>
2023-12-21 19:00:24 +0100

    first push

---
+++ ft_printf.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tblagoev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 13:46:21 by tblagoev          #+#    #+#             */
+/*   Updated: 2023/12/21 17:27:40 by tblagoev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int	print_format(char specifier, va_list ap)
+{
+	int	count;
+
+	count = 0;
+	if (specifier == 'c')
+	{
+		c = va_arg(ap, int);
+		count += write(1, &c, 1);
+	}
+	else if (specifier == 's')
+		count += print_str(va_arg(ap, char *));
+	else if (specifier == 'd' || specifier == 'i')
+		count += print_integer((long)va_arg(ap, int), 10, H_L_BASE);
+	else if (specifier == 'x')
+		count += print_digit(va_arg(ap, unsigned int), 16, H_L_BASE);
+	else if (specifier == 'X')
+		count += print_digit((long)va_arg(ap, unsigned int), 16, H_U_BASE);
+	else if (specifier == 'u')
+		count += print_unsigneddecimal((unsigned int)va_arg(ap, unsigned int),
+				10, H_L_BASE);
+	else if (specifier == '%')
+		return (write(1, "%", 1));
+	else if (specifier == 'p')
+		count += print_mem((unsigned long)va_arg(ap, void *), 16, H_L_BASE);
+	return (count);
+}
+
+int	ft_printf(char const *format, ...)
+{
+	va_list	ap;
+	int		count;
+
+	va_start(ap, format);
+	count = 0;
+	while (*format)
+	{
+		if (format == '%')
+			count += print_format(*(++format), ap);
+		else
+			count += write (1, format, 1);
+		++format;
+	}
+	va_end(ap);
+	return count;
+}
+
+int	main()
+{
+	int	count;
+
+	count = ft_printf("Hello %s\n", "John");
+	printf("The chars writeen are %d\n", count);
+}
+
+/*int	average(int n, ...)
+{
+	va_list	ap;
+	int		total;
+	int		i;
+
+	va_start(ap, n);
+	total = 0;
+	i = 0;
+	while (i < n)
+	{
+		total += va_arg(ap, int);
+		i++;
+	}
+	return ((float)total / n);
+}
+
+int	main()
+{
+	float	average_age;
+
+	average_age = average(3, 10, 15, 20);
+	printf("The average age of those 3 ppl is %f\n", average_age);
+}*/
---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tblagoev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 13:37:51 by tblagoev          #+#    #+#             */
+/*   Updated: 2023/12/01 16:09:56 by tblagoev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
